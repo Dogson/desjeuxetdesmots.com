3e1b5279db59068eefcc5b2cfbadf52f98270106
Gwen <gwenael.girod@gmail.com>
2020-10-09 15:43:16 +0200

    Added pretty checkboxes & "submit" btn to filters

--- src/components/settings/settings.js
+++ src/components/settings/settings.js
@@ -1,24 +1,110 @@
 import React from 'react';
 import {connect} from "react-redux";
 import styles from "./settings.module.scss"
-import {FaCog} from "react-icons/fa";
-import ReactTooltip from "react-tooltip";
+import {FaCog, FaFilter} from "react-icons/fa";
+import Popover from "react-popover";
+import {Checkbox} from 'pretty-checkbox-react';
+import '@djthoms/pretty-checkbox';
+import cx from "classnames";
+import {MEDIA_LOGOS, MEDIA_TYPES} from "../../config/const";
 
 class Settings extends React.Component {
     constructor(props) {
         super(props);
 
         this.state = {
-            isOpen: false
+            isOpen: false,
+            settings: this.props.settings
         }
+
+        this._handleTogglePopover = this._handleTogglePopover.bind(this);
+        this._handleChangeFilter = this._handleChangeFilter.bind(this);
+        this._handleSaveSettings = this._handleSaveSettings.bind(this);
+
+    }
+
+    _handleTogglePopover() {
+        if (this.state.isOpen) {
+            this.setState({settings: this.props.settings})
+        }
+        this.setState({
+            isOpen: !this.state.isOpen
+        })
+    }
+
+    _handleChangeFilter(data, property) {
+        const settings = {...this.state.settings};
+        settings.filters[property][data] = !settings.filters[property][data];
+        this.setState({settings});
+    }
+
+    _handleSaveSettings() {
+
     }
 
 
+    renderPopoverContent() {
+        const {settings} = this.state;
+        return <div className={styles.settingsPopoverContainer}>
+            <div className={styles.blockTitle}>
+                <FaFilter className={styles.icon}/>
+                <span>Filtres</span>
+            </div>
+            <div className={styles.settingsBlock}>
+                <div className={styles.subBlock}>
+                    <div className={styles.subBlockTitle}>
+                        Types de média
+                    </div>
+                    {
+                        MEDIA_TYPES.map(type =>
+                            <div className={styles.filterRow}>
+                                <Checkbox shape="curve" color="#FFC857"
+                                          checked={settings.filters.types[type.dataLabel]}
+                                          onChange={() => this._handleChangeFilter(type.dataLabel, "types")}
+                                >
+                                    <span className={styles.filterName}>{type.name}</span>
+                                </Checkbox>
+
+                            </div>
+                        )
+                    }
+                </div>
+                <div className={styles.subBlock}>
+                    <div className={styles.subBlockTitle}>
+                        Médias
+                    </div>
+                    {
+                        MEDIA_LOGOS.map(media =>
+                            <div className={styles.filterRow}>
+                                <Checkbox shape="curve"
+                                          checked={settings.filters.medias[media.name]}
+                                          onChange={() => this._handleChangeFilter(media.name, "medias")}>
+                                    <span className={styles.filterName}>{media.name}</span>
+                                </Checkbox>
+
+                            </div>
+                        )
+                    }
+                </div>
+
+
+            </div>
+            <button className={styles.btn} onClick={this._handleSaveSettings}>Appliquer</button>
+        </div>
+    }
+
     render() {
+        const {isOpen} = this.state;
         return <div className={styles.settingsContainer}>
-            <FaCog className={styles.adminButton} data-for="settings" data-tip="Paramètres"
-                   onClick={() => this.setState({isOpen: true})}/>
-            <ReactTooltip effect="solid" id="settings" place="left"/>
+            <Popover
+                isOpen={isOpen}
+                onOuterAction={this._handleTogglePopover}
+                place="left"
+                body={this.renderPopoverContent()}
+            >
+                <FaCog className={cx(styles.adminButton, isOpen && styles.active)}
+                       onClick={this._handleTogglePopover}/>
+            </Popover>
         </div>
     }
 }
--- src/reducers/settingsReducer.js
+++ src/reducers/settingsReducer.js
@@ -1,11 +1,30 @@
 import {ACTIONS_SETTINGS} from "../actions/settingsActions";
+import {MEDIA_LOGOS, MEDIA_TYPES} from "../config/const";
+
+const getDefaultMediaFilter = () => {
+    const res = {}
+    MEDIA_LOGOS.sort((x, y) => x.name < y.name ? - 1 : 1).forEach(media => {
+        res[media.name] = true;
+    });
+    return res;
+}
+
+const getDefaultMediaTypeFilter = () => {
+    const res = {}
+    MEDIA_TYPES.sort((x, y) => x.dataLabel < y.dataLabel ? - 1 : 1).forEach(type => {
+        res[type.dataLabel] = true;
+    });
+    return res;
+}
+
+const defaultFilters = {
+    medias: localStorage.getItem('filteredMedias') ? JSON.parse(localStorage.getItem('filteredMedias')) : getDefaultMediaFilter(),
+    types: localStorage.getItem('filteredTypes') ? JSON.parse(localStorage.getItem('filteredTypes')) : getDefaultMediaTypeFilter(),
+}
 
 export default (state = {
     settings: {
-        filters: {
-            medias: localStorage.getItem('filteredMedias') && JSON.parse(localStorage.getItem('filteredMedias')),
-            types: localStorage.getItem('filteredTypes') && JSON.parse(localStorage.getItem('filteredTypes')),
-        }
+        filters: defaultFilters
     }
 }, action) => {
     switch (action.type) {
