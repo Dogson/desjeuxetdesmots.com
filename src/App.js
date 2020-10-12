92e82bbbdd5013519f1cdd74d1e4b2fa526f8602
Gwen <gwenael.girod@gmail.com>
2020-10-12 17:35:06 +0200

    Added a Switch to App Router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {BrowserRouter as Router, Route} from "react-router-dom";
+import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
 import './App.css';
 import Homepage from "./pages/homepage/Homepage";
 import Admin from "./pages/admin/Admin";
@@ -15,10 +15,12 @@ class App extends Component {
     render() {
         return (
             <Router>
-                <Route path="/" exact component={Index}/>
-                <Route path="/admin" exact component={Admin}/>
-                <Route exact path="/game/:gameId" component={GamePage} />
-                <Route component={PageNotFound} />
+                <Switch>
+                    <Route path="/" exact component={Index}/>
+                    <Route path="/admin" exact component={Admin}/>
+                    <Route exact path="/game/:gameId" component={GamePage}/>
+                    <Route component={PageNotFound}/>
+                </Switch>
                 <MediaPlayer/>
 
             </Router>
