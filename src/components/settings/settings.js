cfaef0713df0ccbc193fedf486bce2b3f3da2d2b
Gwen <gwenael.girod@gmail.com>
2020-10-12 15:52:45 +0200

    Added "check all" checkbox to settings form

--- src/components/settings/settings.js
+++ src/components/settings/settings.js
@@ -17,15 +17,44 @@ class Settings extends React.Component {
             isOpen: false,
             mediasFilter: {...this.props.settings.filters.medias},
             typesFilter: {...this.props.settings.filters.types},
-            remember: this.props.settings.remember
+            remember: this.props.settings.remember,
+            shouldCheckAllTypes: true,
+            shouldCheckAllMedias: true
         }
+
 
         this._handleTogglePopover = this._handleTogglePopover.bind(this);
         this._handleChangeMediasFilter = this._handleChangeMediasFilter.bind(this);
         this._handleChangeTypesFilter = this._handleChangeTypesFilter.bind(this);
         this._handleChangeRemember = this._handleChangeRemember.bind(this);
         this._handleSaveSettings = this._handleSaveSettings.bind(this);
+    }
 
+    componentDidMount() {
+        this.setState({
+            shouldCheckAllTypes: this.getShouldCheckAllTypes(this.state.typesFilter),
+            shouldCheckAllMedias: this.getShouldCheckAllMedias(this.state.mediasFilter)
+        })
+    }
+
+    getShouldCheckAllTypes(typesFilter) {
+        let shouldCheckAll = false;
+        Object.keys(typesFilter).forEach((type) => {
+            if (!typesFilter[type]) {
+                shouldCheckAll = true;
+            }
+        })
+        return shouldCheckAll;
+    }
+
+    getShouldCheckAllMedias(mediasFilter) {
+        let shouldCheckAll = false;
+        Object.keys(mediasFilter).forEach((media) => {
+            if (!mediasFilter[media]) {
+                shouldCheckAll = true;
+            }
+        })
+        return shouldCheckAll;
     }
 
     _handleTogglePopover() {
@@ -40,13 +69,47 @@ class Settings extends React.Component {
     _handleChangeMediasFilter(data) {
         const {mediasFilter} = {...this.state};
         mediasFilter[data] = !mediasFilter[data];
-        this.setState({mediasFilter});
+        this.setState({
+            mediasFilter,
+            shouldCheckAllMedias: this.getShouldCheckAllMedias(mediasFilter)
+        });
     }
 
     _handleChangeTypesFilter(data) {
         const {typesFilter} = {...this.state};
         typesFilter[data] = !typesFilter[data];
-        this.setState({typesFilter});
+        this.setState({
+            typesFilter,
+            shouldCheckAllTypes: this.getShouldCheckAllTypes(typesFilter),
+        });
+    }
+
+    _handleSelectAllType() {
+        const {typesFilter} = {...this.state};
+
+        const newTypesFilter = {};
+        Object.keys(typesFilter).forEach((key) => {
+            newTypesFilter[key] = this.state.shouldCheckAllTypes;
+        })
+
+        this.setState({
+            typesFilter: newTypesFilter,
+            shouldCheckAllTypes: this.getShouldCheckAllTypes(newTypesFilter)
+        });
+    }
+
+    _handleSelectAllMedia() {
+        const {mediasFilter} = {...this.state};
+
+        const newMediasFilter = {};
+        Object.keys(mediasFilter).forEach((key) => {
+            newMediasFilter[key] = this.state.shouldCheckAllMedias;
+        })
+
+        this.setState({
+            mediasFilter: newMediasFilter,
+            shouldCheckAllMedias: this.getShouldCheckAllMedias(newMediasFilter)
+        });
     }
 
     _handleSaveSettings() {
@@ -82,7 +145,12 @@ class Settings extends React.Component {
             <div className={styles.settingsBlock}>
                 <div className={styles.subBlock}>
                     <div className={styles.subBlockTitle}>
-                        Types de média
+                        <Checkbox shape="curve" color="#FFC857"
+                                  checked={!this.state.shouldCheckAllTypes}
+                                  onChange={() => this._handleSelectAllType()}
+                        >
+                            <span>Types de média</span>
+                        </Checkbox>
                     </div>
                     {
                         MEDIA_TYPES.sort((x, y) => x.dataLabel < y.dataLabel ? -1 : 1).map(type =>
@@ -100,7 +168,12 @@ class Settings extends React.Component {
                 </div>
                 <div className={styles.subBlock}>
                     <div className={styles.subBlockTitle}>
-                        Médias
+                        <Checkbox shape="curve" color="#FFC857"
+                                  checked={!this.state.shouldCheckAllMedias}
+                                  onChange={() => this._handleSelectAllMedia()}
+                        >
+                            <span>Média</span>
+                        </Checkbox>
                     </div>
                     {
                         MEDIA_LOGOS.sort((x, y) => x.name < y.name ? -1 : 1).map(media =>
