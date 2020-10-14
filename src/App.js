d9ea8b5e704f0015729d55c065ca00587d355599
Girod <gwenael.girod@gmail.com>
2020-10-14 16:39:10 +0200

    Added Google Tag Manager

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Admin from "./pages/admin/Admin";
 import GamePage from "./pages/game/GamePage";
 import MediaPlayer from "./components/mediaPlayerWidgets/mediaPlayerWidgets";
 import {PageNotFound} from "./pages/404/pageNotFound";
+import withTracker from "./components/withTracker/withTracker";
 
 function Index() {
     return <Homepage/>
@@ -16,10 +17,10 @@ class App extends Component {
         return (
             <Router>
                 <Switch>
-                    <Route path="/" exact component={Index}/>
-                    <Route path="/admin" exact component={Admin}/>
-                    <Route exact path="/game/:gameId" component={GamePage}/>
-                    <Route component={PageNotFound}/>
+                    <Route path="/" exact component={withTracker(Index)}/>
+                    <Route path="/admin" exact component={withTracker(Admin)}/>
+                    <Route exact path="/game/:gameId" component={withTracker(GamePage)}/>
+                    <Route component={withTracker(PageNotFound)}/>
                 </Switch>
                 <MediaPlayer/>
 
